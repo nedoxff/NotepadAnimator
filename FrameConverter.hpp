601079a54b6ee37a7f5b588b5599fc9c19ff78fb
undefined <65343244+NedoProgrammer@users.noreply.github.com>
2021-02-04 19:00:25 +0300

    Update FrameConverter.hpp

--- FrameConverter.hpp
+++ FrameConverter.hpp
@@ -16,6 +16,10 @@ public:
     static std::string Convert(const std::filesystem::path& file);
     static void ConvertToGrayscale(Gdiplus::Bitmap* copyTo);
     static void InitializeGdiplus();
+	static long MapNumbers(long x, long in_min, long in_max, long out_min, long out_max)
+	{
+		return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+	}
 };
 
 
